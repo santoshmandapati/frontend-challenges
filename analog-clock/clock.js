df047ccc44bdbb8aabeb2c592375cc4a60a47eef
santoshmandapati <santosh.mandapati@gmail.com>
2021-06-10 19:58:52 -0400

    analog clock implementation

---
+++ analog-clock/clock.js
@@ -0,0 +1,19 @@
+(
+    function() {
+        let secondDial = document.querySelector('.second-dial');
+        let minuteDial = document.querySelector('.minute-dial');
+        let hourDial = document.querySelector('.hour-dial');
+        setInterval(() => {
+            const date = new Date();
+            const seconds = date.getSeconds() * 6;
+            const minutes = date.getMinutes() * 6;
+            const hours = (date.getHours() * 30) + date.getMinutes() / 2;
+    
+            console.log();
+
+            secondDial.style.transform = `rotate(${seconds}deg)`;
+            minuteDial.style.transform = `rotate(${minutes}deg)`;
+            hourDial.style.transform = `rotate(${hours}deg)`;
+        }, 1000);
+    }
+)();
